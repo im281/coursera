c8dab585b67b282235112ccc0334135adbd83615
matiasbargas <matiasbargas@gmail.com>
2014-02-16 19:53:39 -0300

    unit 1: percolation assigment

---
+++ algo1/percolation/src/Percolation.java
@@ -0,0 +1,107 @@
+import java.util.Arrays;
+
+public class Percolation {
+
+    private WeightedQuickUnionUF full;
+
+    private boolean[] opened;
+    private int n;
+
+    /**
+     * create N-by-N grid, with all sites blocked
+     */
+    public Percolation(int N) {
+        n = N;
+        full = new WeightedQuickUnionUF(n * n + 1);
+        prepare();
+    }
+
+    /**
+     * open site (row i, column j) if it is not already
+     */
+    public void open(int i, int j) {
+        if (i < 1 || i > n || j < 1 || j > n)
+            throw new IndexOutOfBoundsException("wrong coordinades");
+
+        int pox = toFlatPox(i, j);
+        if (!opened[pox]) {
+            opened[pox] = true;
+            connectWithAdjacents(i, j, pox);
+        }
+    }
+
+    /**
+     * is site (row i, column j) open?
+     */
+    public boolean isOpen(int i, int j) {
+        if (i < 1 || i > n || j < 1 || j > n)
+            throw new IndexOutOfBoundsException("wrong coordinades");
+
+        return opened[toFlatPox(i, j)];
+    }
+
+    /**
+     * is site (row i, column j) full?
+     */
+    public boolean isFull(int i, int j) {
+        if (i < 1 || i > n || j < 1 || j > n)
+            throw new IndexOutOfBoundsException("wrong coordinades");
+
+        if (!isOpen(i, j))
+            return false;
+
+        if (i == 1)
+            return true;
+
+        return connectedWithRoof(i, j);
+    }
+
+    private boolean connectedWithRoof(int i, int j) {
+        return full.connected(toUFPox(i, j), 0);
+    }
+
+    /**
+     * does the system percolate?
+     */
+    public boolean percolates() {
+        return false;
+    }
+
+    private void prepare() {
+        opened = new boolean[n * n];
+        Arrays.fill(opened, false);
+        for (int col = 1; col <= n; col++) {
+            // init uf's
+            full.union(toUFPox(1, col), 0);
+        }
+    }
+
+    private void connectWithAdjacents(int i, int j, int pox) {
+        doConnect(i > 1, pox, toFlatPox(i - 1, j)); // connect with up
+        doConnect(i < n, pox, toFlatPox(i + 1, j)); // connect with down
+        doConnect(j > 1, pox, toFlatPox(i, j - 1)); // connect with left
+        doConnect(j < n, pox, toFlatPox(i, j + 1)); // connect with right
+    }
+
+    private void doConnect(boolean condition, int fromPox, int toPox) {
+        if (condition && opened[toPox]) {
+            full.union(toUFPox(toPox), toUFPox(fromPox));
+        }
+    }
+
+    /**
+     * utilitaries to transform a grid [n][n] point into a position in a [0,n-1]
+     * array
+     */
+    private int toFlatPox(int i, int j) {
+        return ((i - 1) * n) + (j - 1);
+    }
+
+    private int toUFPox(int i, int j) {
+        return toUFPox(toFlatPox(i, j));
+    }
+
+    private int toUFPox(int flatPox) {
+        return flatPox + 1;
+    }
+}
---
+++ algo1/percolation/src/PercolationStats.java
@@ -0,0 +1,95 @@
+public class PercolationStats {
+    private int n;
+    private int t;
+    private double[] results;
+
+    /**
+     * perform T independent computational experiments on an N-by-N grid
+     */
+    public PercolationStats(int N, int T) {
+        if (N < 1 || T < 1)
+            throw new IllegalArgumentException("cannot be 0");
+
+        n = N;
+        t = T;
+        results = new double[t];
+        run();
+    }
+
+    private void run() {
+        for (int i = 0; i < t; i++) {
+
+            Percolation percolator = new Percolation(n);
+            double attempts = 0;
+            while (!percolator.percolates()) {
+                int fil = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                if (!percolator.isOpen(fil, col)) {
+                    // ignoring already used fils and cols
+                    percolator.open(fil, col);
+                    attempts++;
+                }
+            }
+            double result = attempts / (n * n);
+            results[i] = result;
+        }
+    }
+
+    /**
+     * sample mean of percolation threshold
+     */
+    public double mean() {
+        double threshold = 0;
+        for (double a : results) {
+            threshold += a;
+        }
+        return threshold / t;
+    }
+
+    /**
+     * sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        if (t == 1)
+            return Double.NaN;
+
+        double mean = mean();
+
+        double count = 0.0;
+        for (double result : results) {
+            count += ((result - mean) * (result - mean));
+        }
+
+        return Math.sqrt(count / (t - 1));
+    }
+
+    /**
+     * returns lower bound of the 95% confidence interval
+     */
+    public double confidenceLo() {
+        double mean = mean();
+        double stddev = stddev();
+        return mean - (1.96 * (stddev / Math.sqrt(t)));
+    }
+
+    /**
+     * returns upper bound of the 95% confidence interval
+     */
+    public double confidenceHi() {
+        double mean = mean();
+        double stddev = stddev();
+        return mean + (1.96 * (stddev / Math.sqrt(t)));
+    }
+
+    /**
+     * test client, described below
+     */
+    public static void main(String[] args) {
+        PercolationStats stats = new PercolationStats(1, 10);
+        System.out.println("java PercolationStats 200 100");
+        System.out.println("mean                    =" + stats.mean());
+        System.out.println("stddev                  =" + stats.stddev());
+        System.out.println("95% confidence interval =" + stats.confidenceLo()
+                + "," + stats.confidenceHi());
+    }
+}
